820d92045fe523ea4f4bebc3a392a75237349196
Mano K <manozart5@gmail.com>
2025-05-15 20:41:08 +0530

    v1

---
+++ app.py
@@ -0,0 +1,113 @@
+from flask import Flask, redirect, request, session, render_template
+import requests
+import os
+import urllib.parse
+from datetime import datetime
+from generate_lyrics import generate_lyrics
+from dotenv import load_dotenv
+
+load_dotenv()  
+app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')  # Used to sign session cookies
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://api.spotify.com/v1/'
+SCOPE = 'user-read-playback-state user-read-currently-playing'
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    params={
+        'client_id': CLIENT_ID,
+        'response_type':'code',
+        'redirect_uri': REDIRECT_URI,
+        'scope': SCOPE
+    }
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+    
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return f"Error: {request.args['error']}"
+    
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'redirect_uri': REDIRECT_URI,
+            'grant_type': 'authorization_code',
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+    response = requests.post(TOKEN_URL, data=req_body)
+    token_info = response.json()
+    
+    session['access_token'] = token_info['access_token']
+    session['refresh_token'] = token_info['refresh_token']
+    session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+    
+    return redirect('/home')
+
+@app.route('/home')
+def home():
+    if 'access_token' not in session:
+        return redirect('/login')
+
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh_token')
+
+    return render_template('home.html')
+
+@app.route('/refresh_token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    req_body = {
+        'grant_type': 'refresh_token',
+        'refresh_token': session['refresh_token'],
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }
+    response = requests.post(TOKEN_URL, data=req_body)
+    new_token_info = response.json()
+    session['access_token'] = new_token_info ['access_token']
+    session['expires_at'] = datetime.now().timestamp() + new_token_info ['expires_in']
+    
+    return redirect('/home')
+
+@app.route('/get-lyrics',methods=['POST'])
+def get_lyrics():
+    access_token = session.get('access_token')
+    if not access_token:
+        return redirect('/login')
+    
+    headers = {'Authorization': f'Bearer {access_token}'}
+    spotify_response = requests.get(f"{BASE_URL}me/player/currently-playing", headers=headers)
+    
+    if spotify_response.status_code != 200:
+        return f"Spotify API Error: {spotify_response.status_code} - {spotify_response.text}", 400
+    
+    data = spotify_response.json()
+    if not data or 'item' not in data or data['item'] is None:
+        return "No song is currently playing.", 400
+
+    song_name = data['item']['name']
+    artist_name = data['item']['artists'][0]['name']
+    
+    try:
+        lyrics = generate_lyrics(song_name)
+        return render_template("home.html", lyrics=lyrics)
+    except Exception as e:
+        return render_template("home.html", lyrics=f"Error generating lyrics: {str(e)}")
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
---
+++ generate_lyrics.py
@@ -0,0 +1,47 @@
+import requests
+import re
+import urllib.parse
+from bs4 import BeautifulSoup
+
+def format_lyrics(t):
+    # print(t)
+    match = re.search(r'(Male :|Female :|Chorus :)', t)
+    if not match:
+        return "Unable to parse lyrics structure."
+    lyrics_start = t[match.start():]
+    formatted_lyrics = re.sub(r'(?<!^)(?=[A-Z])', '\n', lyrics_start)
+    return formatted_lyrics
+
+def get_link(query,max_results=3):
+    query = "tamil2lyrics lyrics "+query
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    query_encoded = urllib.parse.quote_plus(query)
+    url = f"https://html.duckduckgo.com/html/?q={query_encoded}"
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    results = []
+    for result in soup.find_all('a', {'class': 'result__a'}, limit=max_results):
+        title = result.get_text()
+        link = result.get('href')
+        results.append({'title': title, 'link': link})
+
+    raw_link = results[0]["link"]
+    parsed = urllib.parse.urlparse("https:" + raw_link)
+    query_params = urllib.parse.parse_qs(parsed.query)
+
+    real_url = query_params.get('uddg', [None])[0]
+    return real_url
+
+def generate_lyrics(song_name):
+    url = get_link(query=song_name)
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content_div = soup.find('div', id='English')  #Change soup based on actual website structure
+    if content_div:
+        lyrics_text = content_div.get_text(separator="\n", strip=True)
+        lyrics = format_lyrics(lyrics_text)
+    else:
+        lyrics = "Lyrics section not found on the lyrics page."
+    return lyrics
